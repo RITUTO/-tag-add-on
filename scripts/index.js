5508fe8540fea5196fe04964550805008ed825a8
RITUTO <144442942+RITUTO@users.noreply.github.com>
2023-11-23 16:49:53 +0900

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,10 @@
 import { EntitySkinIdComponent, ScriptEventCommandMessageAfterEvent, system, world } from "@minecraft/server";//インポート
 import { ModalFormData,ActionFormData,MessageFormData} from "@minecraft/server-ui";
-world.afterEvents.itemUseOn.subscribe(async ev => {
+world.afterEvents.itemUseOn.subscribe(async (/** @type {{ source: any; itemStack: any; }} */ ev) => {
   const { source: player,itemStack:item} = ev;
   
 
-  var ke = (player.getTags().filter(t => t.startsWith("blockput")))//blockput tagがついてるか確認
+  var ke = (player.getTags().filter((/** @type {string} */ t) => t.startsWith("blockput")))//blockput tagがついてるか確認
   if (ke[0] !== undefined){
   player.removetags(`tag @s remove "${ke[0]}"`)
   }
@@ -12,7 +12,7 @@ world.afterEvents.itemUseOn.subscribe(async ev => {
 })          
 
 
-world.afterEvents.playerBreakBlock.subscribe(ev =>{
+world.afterEvents.playerBreakBlock.subscribe((/** @type {{ player: any; brokenBlockPermutation: any; }} */ ev) =>{
       const { player,brokenBlockPermutation } = ev;
         player.runCommandAsync(`scoreboard objectives add "mine:${brokenBlockPermutation.type.id}" dummy `)
         player.runCommandAsync(`scoreboard objectives add "blockminecount" dummy `)
@@ -36,7 +36,7 @@ world.afterEvents.playerBreakBlock.subscribe(ev =>{
         }else{
           taisyou.runCommandAsync(`scoreboard players reset @s sneak`)
         }
-        taisyou.getTags().forEach((tag) => {
+        taisyou.getTags().forEach((/** @type {string} */ tag) => {
           if (tag.startsWith("hastag:")) {
            let b = tag.replace("hastag:", "");
            taisyou.runCommandAsync(`tag @s remove ${tag}`)
@@ -50,7 +50,7 @@ world.afterEvents.playerBreakBlock.subscribe(ev =>{
         }})
   }} , 0)
   
-  world.afterEvents.entityHitEntity.subscribe(ev =>{//ヒット数カウント
+  world.afterEvents.entityHitEntity.subscribe((/** @type {{ damagingEntity: any; hitEntity: any; }} */ ev) =>{//ヒット数カウント
     const entiy = ev.damagingEntity
     const hitentiy = ev.hitEntity
     entiy.runCommandAsync("scoreboard objectives add hitcount dummy")
@@ -59,18 +59,18 @@ world.afterEvents.playerBreakBlock.subscribe(ev =>{
     hitentiy.runCommandAsync("tag @s add hit")
 }
     )
-    world.beforeEvents.chatSend.subscribe(ev => { //チャットが送信されたら
+    world.beforeEvents.chatSend.subscribe((/** @type {{ message: any; sender: any; cancel: boolean; }} */ ev) => { //チャットが送信されたら
       const message = ev.message;
       const sender = ev.sender;
       
-      var ke = (sender.getTags().filter(t => t.startsWith("mute")))//mute tagがついてるか確認
+      var ke = (sender.getTags().filter((/** @type {string} */ t) => t.startsWith("mute")))//mute tagがついてるか確認
       if (ke[0] !== undefined){
       let sp = ke[0]
       let kekkafisp = sp.split(':')
       ev.cancel = true; //チャットをキャンセル
       sender.sendMessage(`${kekkafisp[1]}`)
     }else{
-      var ke = (sender.getTags().filter(t => t.startsWith("blockword")))//muteword tagがついてるか確認
+      var ke = (sender.getTags().filter((/** @type {string} */ t) => t.startsWith("blockword")))//muteword tagがついてるか確認
       if (ke[0] !== undefined){
       let sp = ke[0]
       let kekkafisp = sp.split(':')
@@ -80,7 +80,7 @@ world.afterEvents.playerBreakBlock.subscribe(ev =>{
     }else{
       sender.runCommandAsync("scoreboard objectives add chatcount dummy")
       sender.runCommandAsync(`scoreboard players add @s chatcount 1`)//チャットカウント
-      var ke = (sender.getTags().filter(t => t.startsWith("chat")))//word tagがついてるか確認
+      var ke = (sender.getTags().filter((/** @type {string} */ t) => t.startsWith("chat")))//word tagがついてるか確認
       if (ke[0] !== undefined){
       sender.runCommandAsync(`tag @s remove "${ke[0]}"`)
       }
